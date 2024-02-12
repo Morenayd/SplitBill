3b4060fdd4f3f90eb825890e4c87e432b826cba1
Jesutofunmi <tofdebby@gmail.com>
2024-02-12 12:33:23 +0100

    Initial Commit

---
+++ Split Bills/Split_BillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Split_BillsApp.swift
+//  Split Bills
+//
+//  Created by Jesutofunmi Adewole on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Split_BillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
